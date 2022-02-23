766c7740791bb082832e0760a3b22d78d17464de
collazomathias <mathias.collazo.episcopo@gmail.com>
2022-02-23 14:21:17 -0300

    Añadido frontend para agregar TODOs.

--- Backend/src/main/java/co/com/sofka/kata/crud/TodoController.java
+++ Backend/src/main/java/co/com/sofka/kata/crud/TodoController.java
@@ -1,6 +1,7 @@
 package co.com.sofka.kata.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
     @Autowired
     private TodoService service;

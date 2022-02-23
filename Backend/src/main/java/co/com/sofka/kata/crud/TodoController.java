524fcdfba877d711ba49259f950bd2bc4eed69e1
collazomathias <mathias.collazo.episcopo@gmail.com>
2022-02-23 12:01:50 -0300

    Primer commit, proyecto creado con backend y frontend añadidos. Se trabajará en Springboot (backend) y ReactJS con Hooks (Frontend).

---
+++ Backend/src/main/java/co/com/sofka/kata/crud/TodoController.java
@@ -0,0 +1,44 @@
+package co.com.sofka.kata.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @Autowired
+    private TodoService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<Todo> list(){
+        return service.list();
+    }
+
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId() != null){
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el ID para actualizar.");
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id){
+        service.delete(id);
+    }
+
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id){
+        return service.get(id);
+    }
+}
